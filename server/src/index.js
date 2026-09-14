c9df4a874560cb0b56c3c877c48dbae2fe340bbd
n17rb <140302157+n17rb@users.noreply.github.com>
2026-09-15 00:02:57 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,7 @@ import productRoutes from "./routes/products.js";
 import regionRoutes from "./routes/regions.js";
 import userRoutes from "./routes/users.js";
 import orderRoutes from "./routes/orders.js";
+import tripRoutes from "./routes/trips.js";
 
 dotenv.config();
 
@@ -32,6 +33,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/regions", regionRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/trips", tripRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
